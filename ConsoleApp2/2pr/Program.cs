efd8c12e6e201a8acb3e3a9f736b6b3311522678
Viktor <v.mun@yahoo.com>
2019-02-26 21:20:58 +0100

    2

---
+++ ConsoleApp2/2pr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        {
+            int x;
+            int y = 0;
+            string s;
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("Введите число:");
+                s = Console.ReadLine();
+                int.TryParse(s, out x);
+                if (x > 0 && x % 2 != 0) { y += x; }
+                else if (x == 0) { flag = true; }
+            }
+            while (!flag);
+
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
