afd4e42a48390c884ae84588ba244dfc71b84c82
Viktor <v.mun@yahoo.com>
2019-02-27 20:53:26 +0100

    Dop

--- ConsoleApp2/2pr/Program.cs
+++ ConsoleApp2/2pr/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-class Program
+/*class Program
 {
-    static void Main()
+    private static void Main()
     {
 
         {
@@ -25,4 +25,99 @@ class Program
             Console.ReadLine();
         }
     }
+}*/
+
+/// <summary>
+/// 3.	*Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу, демонстрирующую все разработанные элементы класса.
+//* Добавить свойства типа int для доступа к числителю и знаменателю;
+//* Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа;
+//** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение ArgumentException("Знаменатель не может быть равен 0");
+//*** Добавить упрощение дробей.
+/// </summary>
+class DopZadacha
+{
+    static void Main()
+    {
+        Complex a = new Complex(3, 5);
+        Complex b = new Complex (2, 5);
+
+        Console.WriteLine((a.Plus(b)).ToString());
+        Console.WriteLine((a.Minus(b)).ToString());
+        Console.WriteLine((a.Multi(b)).ToString());
+        Console.ReadKey();
+    }
+}
+
+internal class Complex
+{
+    public double x;
+    public double y;
+
+    public Complex(int a, int b)
+    {
+        this.x = a;
+        this.y = b;
+    }
+    public Complex Plus(Complex a)
+    {
+        Complex res = new Complex(0, 0);
+        if (y == 0 || a.y == 0)
+        {
+            throw new ArgumentException(String.Format("Знаменатель не может быть равен 0"));
+        }
+        else if (y == a.y)
+        {
+            res.x = x + a.x;
+            res.y = y;
+        }
+        else
+        {
+            res.x = x * a.y + a.x * y;
+            res.y = y * a.y;
+        }
+
+           return res;
+    }
+
+    public Complex Minus(Complex a)
+    {
+        Complex res = new Complex(0, 0);
+        if (y == 0 || a.y == 0)
+        {
+            throw new ArgumentException(String.Format("Знаменатель не может быть равен 0"));
+        }
+        else if (y == a.y)
+        {
+            res.x = x - a.x;
+            res.y = y;
+        }
+        else
+        {
+            res.x = x * a.y - a.x * y;
+            res.y = y * a.y;
+        }
+
+        return res;
+    }
+
+    public Complex Multi(Complex a)
+    {
+        Complex res = new Complex(0, 0);
+        if (y == 0 || a.y == 0)
+        {
+            throw new ArgumentException(String.Format("Знаменатель не может быть равен 0"));
+        }
+        else 
+        {
+            res.x = x *a.x;
+            res.y = y*a.y;
+        }
+      
+        return res;
+    }
+    public override string ToString()
+    {
+        return x.ToString() + "/" + y.ToString();
+    }
+
 }
